0431bc6fe4b28ed1b2b098459349dfc603b17116
Dev-Dsouza <lourenco.h.colares@gmail.com>
2025-01-11 21:42:42 -0300

    Bagels finished

--- my_version/01_bagels/main.py
+++ my_version/01_bagels/main.py
@@ -6,10 +6,50 @@ ALLOW_CHARACTERS:bool=False
 ALLOW_UPPER_CHARACTERS:bool=False
 
 def main():
-    secret_number:str=get_secret_num()
-    for _ in range(NUM_CHANCES):
-        user_guess:str=input()
-        print(get_clues(user_guess, secret_number))
+
+##################################################
+#######################################################
+# presentation:
+
+    print('''Bagels, a deductive logic game
+By Al Sweigart with codes improved by Heber-Dsouza
+
+I am thinking of a {}-digit {} with no repeated digits.
+Try to guess what it is. Here are some clues:
+When I say:    That means:
+  Pico         One digit is correct but in the wrong position.
+  Fermi        One digit is correct and in the right position.
+  Bagels       No digit is correct.
+  
+For example, if the secret number was 248 and your guess was 843
+clues would be Fermi Pico.
+'''.format(NUM_LENGTH, 'word' if ALLOW_CHARACTERS else 'number'))
+
+#######################################################
+##################################################
+
+    while True:
+        secret_number: str = get_secret_num()
+        print('I have thought up a number.')
+        print(f"You have {NUM_CHANCES} guesses to get it.")
+        num_guesses:int=1
+        while num_guesses<=NUM_CHANCES:
+            guess:str=''
+            while len(guess)!=NUM_LENGTH or not guess.isdecimal():
+                print(f"Guess #{num_guesses}: ")
+                guess=input('> ')
+            clues:str=get_clues(guess,secret_number)
+            print(clues)
+            num_guesses+=1
+            if guess==secret_number:
+                break
+            if num_guesses>NUM_CHANCES:
+                print('You ran out of guesses.')
+                print(f"The answer was {secret_number}.")
+        print('Do you want to play again? (yes or No)')
+        if not input('> ').lower().startswith('y'):
+            break
+    print('Thanks for playing!')
 
 def get_secret_num()->str:
     list_of_nums=list('0123456789') # more performance than ```list(range(1,10))```
@@ -27,7 +67,6 @@ def get_secret_num()->str:
     return result
 
 def get_clues(guess:str,secret_number:str)->str:
-    print('debug',guess,secret_number)
     if guess == secret_number:
         return 'You got it!'
     clues:list[str]=[]
